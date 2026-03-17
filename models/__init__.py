34035bfa020afb580321d78fb8da6fb6a0c9e939
patrickuibk <patrick.tirler@student.uibk.ac.at>
2026-03-17 09:10:39 +0100

    patch vlfuse

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,16 @@
 from .grounding_pose_head import GroundingPOSEHead
 from .grounding_pose import GroundingPOSE
 from .memoized_bert_model import MemoizedBertModel
-# from .efficient_vit import EfficientViTBackbone
 from .decoder import GroundingPOSETransformerDecoder
+
+try:
+   from . import vlfuse_patch
+except ImportError:
+   pass
 
 __all__ = ['GroundingPOSE',
            'GroundingPOSEHead',
            'MemoizedBertModel',
-        #    'EfficientViTBackbone',
            'GroundingPOSETransformerDecoder']
 
 
